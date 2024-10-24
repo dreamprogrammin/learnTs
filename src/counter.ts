47402aff4907135ed29a0804fe929665965786cd
test <test@MacBook-Pro-Malik.local>
2024-10-24 07:58:29 +0500

    tst

--- src/counter.ts
+++ src/counter.ts
@@ -1,9 +1,46 @@
-export function setupCounter(element: HTMLButtonElement) {
-  let counter = 0
-  const setCounter = (count: number) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
+let bol : boolean = false
+
+let u: undefined = undefined
+
+let n: null = null
+
+const sayHi = () :void => {
+  console.log('Hello, TypeScript!')
+}
+
+const say = () :string => {
+  return 'Hello, TypeScript!'
+}
+
+const createUser = (name: string, age?:number):string | number => `${name}  ${age}`
+
+const b = createUser('Malik')
+
+const createUniUser = (name:string = 'Malik', age:number = 23):string | number => `${name} ${age}`
+
+const a = createUniUser('Malik', 24)  
+
+const createUserSkills = (name:string, ...skills:Array<string>) => `${name} my skills ${skills.join()}`
+
+const k = createUserSkills('Malik', 'html','js','css')
+
+type People = {
+  name: string,
+  age:number,
+  nickName?: string,
+  hobbies?: () => string
+}
+
+const student: People = {
+  name: 'Malik',
+  age: 23,
+  nickName: 'olod'
+}
+
+const person: People = {
+  name: 'Malik',
+  age: 23,
+  getName():string {
+    return `${this.name} and ${this.age}`
   }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
 }
